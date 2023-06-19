7823bb48940efc8cdddf1e94947b4f951910a7d5
skearya <77034153+skearya@users.noreply.github.com>
2023-06-18 22:59:52 -0400

    a

--- src/routes/(anime)/[animeId]/[providerId]/[watchId]/[episode]/+page.server.ts
+++ src/routes/(anime)/[animeId]/[providerId]/[watchId]/[episode]/+page.server.ts
@@ -8,18 +8,53 @@ export const load = (async ({ params }) => {
 
 	if (!episode) throw redirect(303, `/search`);
 
-	let response: SourceInfo;
-
-	try {
-		response = await api(
-			`sources?providerId=${providerId}&watchId=${watchId}&episode=${episode}&id=${animeId}&apikey=${API_KEY}`,
-			{ timeout: 4500 }
-		).json<SourceInfo>();
-	} catch {
-		throw error(404, 'Error fetching episode sources');
+	async function fetchSource() {
+		try {
+			return await api(
+				`sources?providerId=${providerId}&watchId=${watchId}&episode=${episode}&id=${animeId}&apikey=${API_KEY}`,
+				{
+					timeout: 4500
+				}
+			).json<SourceInfo>();
+		} catch (e: any) {
+			throw error(404, {
+				message: 'Error fetching episode sources',
+				info: e.message
+			});
+		}
 	}
 
-	return response;
+	async function fetchInfo() {
+		try {
+			return await api(`info/${animeId}?apikey=${API_KEY}`, {
+				timeout: 3000
+			}).json<AnimeInfo>();
+		} catch (e: any) {
+			throw error(404, {
+				message: 'Error fetching anime info',
+				info: e.message
+			});
+		}
+	}
+
+	async function fetchEpisodes() {
+		try {
+			return await api(`episodes/${animeId}?apikey=${API_KEY}`, {
+				timeout: 3000
+			}).json<EpisodeData[]>();
+		} catch (e: any) {
+			throw error(404, {
+				message: 'Error fetching episode info',
+				info: e.message
+			});
+		}
+	}
+
+	return {
+		source: fetchSource(),
+		info: fetchInfo(),
+		episodes: fetchEpisodes()
+	};
 }) satisfies PageServerLoad;
 
 interface SourceInfo {
@@ -31,7 +66,7 @@ interface SourceInfo {
 }
 
 interface Headers {
-	Referer: string;
+	Referer?: string;
 }
 
 interface Intro {
@@ -42,4 +77,105 @@ interface Intro {
 interface Source {
 	url: string;
 	quality: string;
+}
+
+interface AnimeInfo {
+	id: string;
+	malId: string;
+	slug: string;
+	kitsuId: string;
+	tvdbId: string;
+	coverImage: string;
+	bannerImage: string;
+	trailer: string;
+	status: string;
+	season: string;
+	title: Title;
+	currentEpisode?: any;
+	mappings: Mapping[];
+	synonyms: string[];
+	countryOfOrigin: string;
+	description: string;
+	duration: number;
+	color: string;
+	year: number;
+	rating: Rating;
+	popularity: Rating;
+	type: string;
+	format: string;
+	relations: Relation[];
+	totalEpisodes: number;
+	genres: string[];
+	tags: string[];
+	episodes: Episodes;
+	averageRating: number;
+	averagePopularity: number;
+	artwork: Artwork[];
+}
+
+interface Artwork {
+	img: string;
+	type: string;
+	providerId: string;
+}
+
+interface Episodes {
+	data: any[];
+	updatedAt: number;
+}
+
+interface Relation {
+	id: number;
+	data: Data;
+	type: string;
+}
+
+interface Data {
+	id: number;
+	type: string;
+	title: Title2;
+	format: string;
+	status: string;
+	coverImage: CoverImage;
+	bannerImage: string;
+}
+
+interface CoverImage {
+	large: string;
+}
+
+interface Title2 {
+	userPreferred: string;
+}
+
+interface Rating {
+	mal: number;
+	kitsu: number;
+	anilist: number;
+}
+
+interface Mapping {
+	id: string;
+	providerId: string;
+	similarity: number;
+}
+
+interface Title {
+	native: string;
+	romaji: string;
+	english: string;
+}
+
+interface EpisodeData {
+	providerId: string;
+	episodes: Episode[];
+}
+
+interface Episode {
+	id: string;
+	number: number;
+	title: string;
+	isFiller: boolean;
+	img?: string;
+	hasDub: boolean;
 }
--- src/routes/(anime)/[animeId]/[providerId]/[watchId]/[episode]/+page.svelte
+++ src/routes/(anime)/[animeId]/[providerId]/[watchId]/[episode]/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { PUBLIC_PROXY } from '$env/static/public';
+	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import toast from 'svelte-french-toast';
+	import { Separator } from '$components/ui/separator';
 	import HLS from 'hls.js';
 	import { defineCustomElements } from 'vidstack/elements';
 	import 'vidstack/styles/defaults.css';
@@ -12,7 +14,7 @@
 	let src: string;
 	let usingProxy: boolean = false;
 
-	for (const source of data.sources) {
+	for (const source of data.source.sources) {
 		if (source.quality == 'default' || source.quality == 'auto') {
 			src = source.url;
 		}
@@ -39,7 +41,7 @@
 				player.src = {
 					src: `${PUBLIC_PROXY}m3u8-proxy?url=${encodeURIComponent(
 						src
-					)}&headers=${encodeURIComponent(JSON.stringify(data.headers || {}))}`,
+					)}&headers=${encodeURIComponent(JSON.stringify(data.source.headers || {}))}`,
 					type: 'application/x-mpegurl'
 				};
 
@@ -49,11 +51,62 @@
 	});
 </script>
 
-<section class="mx-auto max-w-screen-2xl">
-	<media-player {src} aspect-ratio="16/9" crossorigin type="application/x-mpegurl">
-		<media-outlet />
+<section
+	class="mx-auto min-h-screen max-w-screen-xl overflow-hidden border-b bg-popover xl:border-x"
+>
+	<media-player
+		title={`${data.info.title.romaji} - Episode ${$page.params.episode}`}
+		{src}
+		aspect-ratio="16/9"
+		crossorigin
+		type="application/x-mpegurl"
+	>
+		<media-outlet>
+			{#if data.source.subtitles[0]}
+				<track
+					src={data.source.subtitles[0].url}
+					kind="subtitles"
+					label={data.source.subtitles[0].lang}
+					srclang="en-US"
+					default
+					data-type="vtt"
+				/>
+			{/if}
+		</media-outlet>
 		<media-community-skin />
 	</media-player>
+
+	<div class="p-4 md:p-6">
+		<div class="flex items-center justify-between">
+			<div class="space-y-1">
+				<h1 class="text-2xl font-semibold tracking-tight md:text-3xl">{data.info.title.romaji}</h1>
+				<h1 class="text-md capitalize text-muted-foreground md:text-lg">
+					{data.info.season}
+					{data.info.year}
+				</h1>
+			</div>
+
+			<h1 class="text-md ml-3 whitespace-nowrap text-muted-foreground md:text-lg">
+				Episode {$page.params.episode}
+			</h1>
+		</div>
+
+		<Separator class="my-4" />
+
+		<!-- <div class="flex gap-2 overflow-x-scroll">
+			{#each data.episodes[0].episodes as episode}
+				<a
+					href="/"
+					id={String(episode.number)}
+					class={`rounded-sm ${
+						episode.number == Number($page.params.episode) ? 'bg-primary text-black' : 'bg-muted'
+					} px-4 py-2 text-muted-foreground transition-all hover:bg-primary hover:text-black`}
+				>
+					{episode.number}
+				</a>
+			{/each}
+		</div> -->
+	</div>
 </section>
 
 <style>
